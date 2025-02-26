fa1d5198788a38637834b4fe36948f27a83251e7
ice02 <{ID}+{username}@users.noreply.github.com>
2025-02-26 18:18:42 +0100

    Ajout de la prise en charge de la localisation
    Implémentation de la localisation dans l'application en injectant `IStringLocalizer` dans plusieurs composants, tels que `MainLayout`, `InvokeApi`, `UserInfos`, et `LoginDisplay`. Ajout d'un sélecteur de culture dans `MainLayout` pour permettre aux utilisateurs de choisir leur langue. Création de fichiers de ressources `.resx` pour les traductions en français, incluant des messages d'autorisation et des informations utilisateur. Mise à jour de la configuration du programme pour activer la journalisation avec Serilog et gérer la localisation des requêtes HTTP.

--- ProxyChecker/Program.cs
+++ ProxyChecker/Program.cs
@@ -1,14 +1,23 @@
 using Microsoft.AspNetCore.Authentication.Negotiate;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
 using MudBlazor.Services;
 using ProxyChecker.Components;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+
+//Add support to logging with SERILOG
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddAuthentication(NegotiateDefaults.AuthenticationScheme).AddNegotiate();
 builder.Services.AddAuthorization(options => options.FallbackPolicy = options.DefaultPolicy);
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
+
+builder.Services.AddLocalization( options => options.ResourcesPath = "Resources");
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -24,6 +33,17 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+//Add support to logging request with SERILOG
+app.UseSerilogRequestLogging();
+
+string[] supportedCultures = ["en-US", "fr-FR"];
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+
+app.UseRequestLocalization(localizationOptions);
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
@@ -34,5 +54,23 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGet("Culture/Set", async (HttpRequest request, [FromQuery] string culture, [FromQuery] string redirectUri) =>
+{
+    if (culture != null)
+    {
+        request.HttpContext.Response.Cookies.Append(
+            CookieRequestCultureProvider.DefaultCookieName,
+            CookieRequestCultureProvider.MakeCookieValue(
+             new RequestCulture(culture, culture)),
+              new CookieOptions
+              {
+                  Path = "/",
+                  Expires = DateTime.Now.AddYears(1)
+              });
+    }
+
+    return Results.LocalRedirect(redirectUri);
+});
 
 app.Run();
