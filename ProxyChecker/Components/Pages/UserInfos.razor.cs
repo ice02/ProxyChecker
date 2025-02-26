0e4c379b5c0b7186bb7ccec878c19b861df3d86c
ice02 <{ID}+{username}@users.noreply.github.com>
2025-02-26 18:57:27 +0100

    Refactor UserInfos pour gérer l'authentification
    Suppression de l'injection directe de `AuthenticationStateProvider` dans `UserInfos.razor`.
    Réorganisation du code dans `UserInfos.razor.cs` pour inclure les espaces de noms nécessaires et définir la classe comme partielle.
    L'injection de `AuthenticationStateProvider` est maintenant nullable, et la logique de récupération de l'identité de l'utilisateur a été mise à jour pour vérifier la nullité de `AuthenticationStateProvider` avant d'appeler `GetAuthenticationStateAsync`.
    Modification de `OnAfterRenderAsync` pour appeler `StateHasChanged()` uniquement après avoir récupéré l'identité de l'utilisateur.

---
+++ ProxyChecker/Components/Pages/UserInfos.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+using System.Security.Principal;
+
+namespace ProxyChecker.Components.Pages
+{
+    public partial class UserInfos : ComponentBase
+    {
+        [Inject]
+        AuthenticationStateProvider? AuthenticationStateProvider { get; set; }
+
+        WindowsIdentity? identity = null;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+
+            logger.LogDebug($"Culture : {CultureInfo.CurrentCulture.NativeName}");
+
+            var userinfo = Localizer.GetString("UserInfo");
+
+            if (identity == null && AuthenticationStateProvider != null)
+            {
+                var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+                identity = authState.User.Identity as WindowsIdentity;
+                StateHasChanged();
+            }
+        }
+    }
+}
